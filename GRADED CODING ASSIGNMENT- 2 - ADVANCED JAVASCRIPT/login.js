c8103236fe97243380209227d2a3ab2340798ac8
Sachin K Thakur <60362013+sachinkthakur60@users.noreply.github.com>
2022-10-23 21:12:07 +0530

    GRADED CODING ASSIGNMENT- 2 - ADVANCED JAVASCRIPT SOLUTION
    GRADED CODING ASSIGNMENT- 2 - ADVANCED JAVASCRIPT SOLUTION

---
+++ GRADED CODING ASSIGNMENT- 2 - ADVANCED JAVASCRIPT/login.js
@@ -0,0 +1,17 @@
+function login() {
+    let username = document.getElementById('user-input').value;
+    let password = document.getElementById('password-input').value;
+
+    window.localStorage.setItem('username', username);
+    window.localStorage.setItem('password', password);
+
+    if(username === 'sachin' && password === 'pass123') {
+        window.location = 'resume.html';
+    } else {
+        document.getElementById('invalid-login').style.display = 'block';
+        document.getElementById('user-input').value = ''
+        document.getElementById('password-input').value = '';
+    }
+}
+
+autofill()
